737828029b1c0cc1efb5290b5c45be68c211d086
Code Guys <codeguys@MacBook-Pro-van-Code.local>
2024-05-07 13:50:08 +0200

    init springboot

---
+++ src/main/java/com/yer/slp/SocialLearningApplication.java
@@ -0,0 +1,13 @@
+package com.yer.slp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yer/slp/SocialLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yer.slp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
